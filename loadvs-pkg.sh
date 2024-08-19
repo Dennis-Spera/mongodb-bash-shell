2f091da6e90bc3ede3e8dca5072b519768f86940
kadmin <dennis.spera@gmail.com>
2024-08-19 10:04:41 -0400

    add core 8-19-1

--- loadvs-pkg.sh
+++ loadvs-pkg.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-if ["$HOSTTYPE" == "arm64"] 2> /dev/null; then
+export directory=$PWD
 
-      export directory=$PWD
-      code () {
-            VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*
-      } 
-     
-else
-      export directory=$PWD
-fi
-
-code -r $directory --args file loadvs-pkg.sh appName.py docsExamined.py mkRSsh.py phead.py\
+bash code.sh -r $directory --args file loadvs-pkg.sh appName.py docsExamined.py mkRSsh.py phead.py\
                           avgMillis.py drivers.py mkSetup.sh queryHash.py bytesRead.py formatOne.py\
                           mloginfo.py README.md code.sh jsonFetcher.py mplot.py requirements.txt\
                           collScans.py keysExamined.py mplotqueries.py vscode.py convertLogs.py\
-                          millis.py nsIndexes.py countCommands.py mkConsh.py ns.py 2> /dev/null
+                          millis.py nsIndexes.py countCommands.py mkConsh.py ns.py 
